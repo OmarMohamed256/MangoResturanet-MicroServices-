f472fecf51e3a47d456e787846a5b054df394e8a
OmarMohamed256 <omarmohamed2342@outlook.com>
2022-11-27 23:58:54 +0200

    Add project files.

---
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,107 @@
+﻿using Mango.Services.ProductAPI.Models.Dto;
+using Mango.Services.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ProductAPI.Controllers
+{
+    [Route("api/products")]
+    public class ProductAPIController : ControllerBase
+    {
+        protected ResponseDto _response;
+        private IProductRepository _productRepositry;
+        public ProductAPIController(IProductRepository productRepositry)
+        {
+            this._response= new ResponseDto();
+            _productRepositry = productRepositry;
+        }
+
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            try
+            {
+                IEnumerable<ProductDto> productDtos = await _productRepositry.GetProducts();
+                _response.Result = productDtos;
+            }
+            catch (Exception ex)
+            {
+
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                    = new List<string> { ex.ToString() };
+            }
+            return _response;
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<object> Get(int id)
+        {
+            try
+            {
+                ProductDto productDto = await _productRepositry.GetProductById(id);
+                _response.Result = productDto;
+            }
+            catch (Exception ex)
+            {
+
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                    = new List<string> { ex.ToString() };
+            }
+            return _response;
+        }
+        [HttpPost]
+        public async Task<object> Post([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                ProductDto NewproductDto = await _productRepositry.CreateUpdateProduct(productDto);
+                _response.Result = NewproductDto;
+            }
+            catch (Exception ex)
+            {
+
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                    = new List<string> { ex.ToString() };
+            }
+            return _response;
+        }
+        [HttpPut]
+        public async Task<object> Put([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                ProductDto NewproductDto = await _productRepositry.CreateUpdateProduct(productDto);
+                _response.Result = NewproductDto;
+            }
+            catch (Exception ex)
+            {
+
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                    = new List<string> { ex.ToString() };
+            }
+            return _response;
+        }
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<object> Delete(int id)
+        {
+            try
+            {
+                bool isSucess = await _productRepositry.DeleteProduct(id);
+                _response.Result = isSucess;
+            }
+            catch (Exception ex)
+            {
+
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                    = new List<string> { ex.ToString() };
+            }
+            return _response;
+        }
+    }
+
+}
