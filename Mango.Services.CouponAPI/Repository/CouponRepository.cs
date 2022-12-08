bffaf517c753b80ed8e5b531140e98ca8b3b215a
OmarMohamed256 <omarmohamed2342@outlook.com>
2022-12-08 18:00:47 +0200

    Setup Coupon Api Service

---
+++ Mango.Services.CouponAPI/Repository/CouponRepository.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.DbContexts;
+using Mango.Services.CouponAPI.Models.Dto;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.Repository
+{
+    public class CouponRepository : ICouponRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IMapper _mapper;
+        public CouponRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            _mapper= mapper;
+            _db = db;
+        }
+
+        public async Task<CouponDto> GetCouponByCode(string couponCode)
+        {
+            var couponFromDb = await _db.Coupons.FirstOrDefaultAsync(c => c.CouponCode == couponCode);
+            return _mapper.Map<CouponDto>(couponFromDb);
+        }
+    }
+}
